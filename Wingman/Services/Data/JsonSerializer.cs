dbcf3cdb0f8db6f95fae40989c8a5ee8f8f80119
Aleksbgbg <kozichka01@abv.bg>
2019-06-13 09:20:32 +0100

    [JsonSerializer] Add default implementation of IDataSerializer.

---
+++ Wingman/Services/Data/JsonSerializer.cs
@@ -0,0 +1,20 @@
+﻿namespace Wingman.Services.Data
+{
+    using Newtonsoft.Json;
+
+    /// <summary> Default implementation of <see cref="IDataSerializer"/>. Converts to JSON via Newtonsoft.Json. </summary>
+    public class JsonSerializer : IDataSerializer
+    {
+        /// <inheritdoc/>
+        public string Serialize(object data)
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+
+        /// <inheritdoc/>
+        public T Deserialize<T>(string data)
+        {
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+    }
+}
