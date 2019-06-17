5d334537a65acaf2ce7b81edff4ac3e422482dd0
Aleksbgbg <kozichka01@abv.bg>
2019-06-17 18:23:10 +0100

    [DataServiceFactory] Implement factory for DataService.

---
+++ Wingman/Services/Data/DataServiceFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace Wingman.Services.Data
+{
+    /// <summary> Factory that creates a default implementation of <see cref="IDataService"/> which saves to %AppData%/YourAppName using JSON formatting. </summary>
+    public static class DataServiceFactory
+    {
+        public static IDataService Create(string appName)
+        {
+            return new DataService(new JsonSerializer(),
+                                   new AppDataStore(new AppNameProvider(appName),
+                                                    new DirectoryManipulator(),
+                                                    new FileManipulator()
+                                   )
+            );
+        }
+    }
+}
