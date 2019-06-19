a08882755581b34b7ef69e584c25b77e1242e9df
Aleksbgbg <kozichka01@abv.bg>
2019-06-19 12:19:38 +0100

    [DependencyRetrieverBase] Add convenience base class for implementing IDependencyRetriever.

---
+++ Wingman/Container/DependencyRetrieverBase.cs
@@ -0,0 +1,31 @@
+﻿namespace Wingman.Container
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary> Convenience base class for implementing <see cref="IDependencyRetriever"/>. </summary>
+    public abstract class DependencyRetrieverBase : IDependencyRetriever
+    {
+        /// <inheritdoc/>
+        public TService GetInstance<TService>(string key = null)
+        {
+            return (TService)GetInstance(typeof(TService), key);
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<TService> GetAllInstances<TService>()
+        {
+            return GetAllInstances(typeof(TService)).Cast<TService>();
+        }
+
+        /// <inheritdoc/>
+        public abstract object GetInstance(Type service, string key = null);
+
+        /// <inheritdoc/>
+        public abstract IEnumerable<object> GetAllInstances(Type service);
+
+        /// <inheritdoc/>
+        public abstract void BuildUp(object instance);
+    }
+}
