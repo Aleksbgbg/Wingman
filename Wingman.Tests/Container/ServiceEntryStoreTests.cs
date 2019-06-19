d15aa7a808acf97aac6e0e2f70cb4da4ffd0fac2
Aleksbgbg <kozichka01@abv.bg>
2019-06-19 13:05:20 +0100

    [ServiceEntryStore] Implement interface.

---
+++ Wingman.Tests/Container/ServiceEntryStoreTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Wingman.Tests.Container
+{
+    using Wingman.Container;
+
+    using Xunit;
+
+    public class ServiceEntryStoreTests
+    {
+        private const string DefaultServiceKey = "Key";
+
+        private readonly ServiceEntryStore _serviceEntryStore;
+
+        public ServiceEntryStoreTests()
+        {
+            _serviceEntryStore = new ServiceEntryStore();
+        }
+
+        [Fact]
+        public void TestServiceIsNotRegisteredByDefault()
+        {
+            bool hasHandler = _serviceEntryStore.HasHandler(DefaultServiceEntry);
+
+            Assert.False(hasHandler);
+        }
+
+        [Fact]
+        public void TestHasHandlerAfterRegistering()
+        {
+            _serviceEntryStore.InsertHandler(DefaultServiceEntry, null);
+
+            Assert.True(_serviceEntryStore.HasHandler(DefaultServiceEntry));
+        }
+
+        [Fact]
+        public void TestHasHandlerAfterRemoving()
+        {
+            _serviceEntryStore.InsertHandler(DefaultServiceEntry, null);
+            _serviceEntryStore.RemoveHandler(DefaultServiceEntry);
+
+            Assert.False(_serviceEntryStore.HasHandler(DefaultServiceEntry));
+        }
+
+        private static ServiceEntry DefaultServiceEntry => new ServiceEntry(typeof(IService), DefaultServiceKey);
+
+        private interface IService { }
+    }
+}
---
+++ Wingman/Container/ServiceEntryStore.cs
@@ -0,0 +1,24 @@
+﻿namespace Wingman.Container
+{
+    using System.Collections.Generic;
+
+    internal class ServiceEntryStore : IServiceEntryStore
+    {
+        private readonly Dictionary<ServiceEntry, IServiceLocationStrategy> _handlers = new Dictionary<ServiceEntry, IServiceLocationStrategy>();
+
+        public bool HasHandler(ServiceEntry serviceEntry)
+        {
+            return _handlers.ContainsKey(serviceEntry);
+        }
+
+        public void InsertHandler(ServiceEntry serviceEntry, IServiceLocationStrategy serviceLocationStrategy)
+        {
+            _handlers[serviceEntry] = serviceLocationStrategy;
+        }
+
+        public void RemoveHandler(ServiceEntry serviceEntry)
+        {
+            _handlers.Remove(serviceEntry);
+        }
+    }
+}
