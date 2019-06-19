380fa096a9cb4a663c3fd25259c60fe4d1ec1bb0
Aleksbgbg <kozichka01@abv.bg>
2019-06-19 17:05:33 +0100

    [HandlerStrategy] Implement handler function location strategy.

---
+++ Wingman.Tests/Container/HandlerStrategyTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Wingman.Tests.Container
+{
+    using Moq;
+
+    using Wingman.Container;
+
+    using Xunit;
+
+    public class HandlerStrategyTests
+    {
+        private readonly Mock<IDependencyRetriever> _dependencyRetrieverMock;
+
+        public HandlerStrategyTests()
+        {
+            _dependencyRetrieverMock = new Mock<IDependencyRetriever>();
+        }
+
+        [Fact]
+        public void TestReturnsHandlerResult()
+        {
+            object expectedObject = new object();
+
+            object actualObject = new HandlerStrategy(_ => expectedObject).LocateService(null);
+
+            Assert.Same(expectedObject, actualObject);
+        }
+
+        [Fact]
+        public void TestPassesDependencyRetrieverArgument()
+        {
+            IDependencyRetriever actualRetriever = null;
+
+            new HandlerStrategy(passedInRetriever => actualRetriever = passedInRetriever).LocateService(_dependencyRetrieverMock.Object);
+
+            Assert.Same(_dependencyRetrieverMock.Object, actualRetriever);
+        }
+    }
+}
---
+++ Wingman/Container/HandlerStrategy.cs
@@ -0,0 +1,19 @@
+﻿namespace Wingman.Container
+{
+    using System;
+
+    public class HandlerStrategy : IServiceLocationStrategy
+    {
+        private readonly Func<IDependencyRetriever, object> _handler;
+
+        public HandlerStrategy(Func<IDependencyRetriever, object> handler)
+        {
+            _handler = handler;
+        }
+
+        public object LocateService(IDependencyRetriever dependencyRetriever)
+        {
+            return _handler(dependencyRetriever);
+        }
+    }
+}
