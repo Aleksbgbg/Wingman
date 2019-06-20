3a5d978ea1ebe5a204accfc7a0f5a4827d76a29e
Aleksbgbg <kozichka01@abv.bg>
2019-06-20 13:34:14 +0100

    [SingletonStrategy] Implement singleton strategy to instantiate service on first call (via IDiStrategy) and cache subsequently. Frees up DiStrategy on first call.

---
+++ Wingman.Tests/Container/Strategies/SingletonStrategyTests.cs
@@ -0,0 +1,71 @@
+﻿namespace Wingman.Tests.Container.Strategies
+{
+    using System.Collections.Generic;
+
+    using Moq;
+
+    using Wingman.Container.Strategies;
+
+    using Xunit;
+
+    public class SingletonStrategyTests
+    {
+        private readonly Mock<IDiStrategy> _diStrategyMock;
+
+        private readonly SingletonStrategy _singletonStrategy;
+
+        public SingletonStrategyTests()
+        {
+            _diStrategyMock = new Mock<IDiStrategy>();
+            _diStrategyMock.Setup(strategy => strategy.LocateService())
+                           .Returns(new object());
+
+            _singletonStrategy = new SingletonStrategy(_diStrategyMock.Object);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        public void TestReturnsSameObject(int callTimes)
+        {
+            object expectedValue = new object();
+            SetupLocateServiceReturns(expectedValue);
+
+            IEnumerable<object> invocationResults = CallLocateServiceMultipleTimes(callTimes);
+
+            Assert.All(invocationResults, actualValue => Assert.Same(expectedValue, actualValue));
+        }
+
+        [Theory]
+        [InlineData(10)]
+        public void TestCallsLocateServiceFirstTimeButNotSubsequently(int subsequentCalls)
+        {
+            _singletonStrategy.LocateService();
+            CallLocateServiceMultipleTimes(subsequentCalls);
+
+            VerifyLocateDiServiceCalledOnce();
+        }
+
+        private void SetupLocateServiceReturns(object obj)
+        {
+            _diStrategyMock.Setup(strategy => strategy.LocateService())
+                           .Returns(obj);
+        }
+
+        private List<object> CallLocateServiceMultipleTimes(int callTimes)
+        {
+            List<object> callResults = new List<object>();
+
+            for (int call = 0; call < callTimes; ++call)
+            {
+                callResults.Add(_singletonStrategy.LocateService());
+            }
+
+            return callResults;
+        }
+
+        private void VerifyLocateDiServiceCalledOnce()
+        {
+            _diStrategyMock.Verify(strategy => strategy.LocateService(), Times.Once);
+        }
+    }
+}
---
+++ Wingman/Container/Strategies/SingletonStrategy.cs
@@ -0,0 +1,25 @@
+﻿namespace Wingman.Container.Strategies
+{
+    internal class SingletonStrategy : IServiceLocationStrategy
+    {
+        private IDiStrategy _diStrategy;
+
+        private object _service;
+
+        internal SingletonStrategy(IDiStrategy diStrategy)
+        {
+            _diStrategy = diStrategy;
+        }
+
+        public object LocateService()
+        {
+            if (_service == null)
+            {
+                _service = _diStrategy.LocateService();
+                _diStrategy = null;
+            }
+
+            return _service;
+        }
+    }
+}
