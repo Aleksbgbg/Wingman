4acc4037c1f370c1b73534cdc4f6f7278bbdfa14
Aleksbgbg <kozichka01@abv.bg>
2019-06-21 17:45:55 +0100

    [ArgumentBuilderFactory] Implement regular factory to return DiArgumentBuilder.

---
+++ Wingman/DI/ArgumentBuilderFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Wingman.DI
+{
+    using Wingman.Container;
+
+    internal class ArgumentBuilderFactory : IArgumentBuilderFactory
+    {
+        private readonly IDependencyRetriever _dependencyRetriever;
+
+        public ArgumentBuilderFactory(IDependencyRetriever dependencyRetriever)
+        {
+            _dependencyRetriever = dependencyRetriever;
+        }
+
+        public IArgumentBuilder CreateBuilderFor(IConstructor constructor)
+        {
+            return new DiArgumentBuilder(_dependencyRetriever, constructor);
+        }
+    }
+}
