b48ae7598d8018715d283a3ce81a41f296121d99
Aleksbgbg <kozichka01@abv.bg>
2019-06-21 12:36:54 +0100

    [ConstructorCandidateEvaluator] Implement to look for the smallest public instance constructor.

---
+++ Wingman/DI/ConstructorCandidateEvaluator.cs
@@ -0,0 +1,31 @@
+﻿namespace Wingman.DI
+{
+    using System;
+    using System.Linq;
+
+    using Wingman.Utilities;
+
+    internal class ConstructorCandidateEvaluator : IConstructorCandidateEvaluator
+    {
+        private readonly IConstructorQueryProvider _constructorQueryProvider;
+
+        public ConstructorCandidateEvaluator(IConstructorQueryProvider constructorQueryProvider)
+        {
+            _constructorQueryProvider = constructorQueryProvider;
+        }
+
+        public IConstructor FindBestConstructorForDi(Type implementation)
+        {
+            IConstructor matchingConstructor =  _constructorQueryProvider.QueryPublicInstanceConstructors(implementation)
+                                                                         .OrderBy(constructor => constructor.ParameterCount)
+                                                                         .FirstOrDefault();
+
+            if (matchingConstructor == null)
+            {
+                ThrowHelper.Throw.ConstructorCandidateEvaluator.NoPublicInstanceConstructors(implementation);
+            }
+
+            return matchingConstructor;
+        }
+    }
+}
---
+++ Wingman/Utilities/ThrowHelper.ConstructorCandidateEvaluator.cs
@@ -0,0 +1,18 @@
+﻿namespace Wingman.Utilities
+{
+    using System;
+
+    internal static partial class ThrowHelper
+    {
+        internal static partial class Throw
+        {
+            internal static class ConstructorCandidateEvaluator
+            {
+                internal static void NoPublicInstanceConstructors(Type implementation)
+                {
+                    InvalidOperationException($"There are no public instance constructors for {implementation.Name}.");
+                }
+            }
+        }
+    }
+}
