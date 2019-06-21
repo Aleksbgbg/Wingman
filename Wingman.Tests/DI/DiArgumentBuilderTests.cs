46e733aa57137a6cef0ca2c03b532441b752eb61
Aleksbgbg <kozichka01@abv.bg>
2019-06-21 17:43:42 +0100

    [DiArgumentBuilder] Add argument builder that simply injects dependencies.

---
+++ Wingman.Tests/DI/DiArgumentBuilderTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Wingman.Tests.DI
+{
+    using Moq;
+
+    using Wingman.Container;
+    using Wingman.DI;
+    using Wingman.Tests.Helpers.DI;
+
+    using Xunit;
+
+    public class DiArgumentBuilderTests
+    {
+        private readonly Mock<IDependencyRetriever> _dependencyRetrieverMock;
+
+        private readonly Mock<IConstructor> _constructorMock;
+
+        public DiArgumentBuilderTests()
+        {
+            _dependencyRetrieverMock = new Mock<IDependencyRetriever>();
+
+            _constructorMock = new Mock<IConstructor>();
+        }
+
+        [Fact]
+        public void ResolvesDependenciesBasedOnArgumentTypes()
+        {
+            object[] dependencies = SetupDependencies(3);
+
+            object[] arguments = ResolveDependencies();
+
+            Assert.Equal(dependencies, arguments);
+        }
+
+        private object[] SetupDependencies(int count)
+        {
+            return DiHelper.SetupDependencies(_constructorMock, _dependencyRetrieverMock, count);
+        }
+
+        private object[] ResolveDependencies()
+        {
+            return new DiArgumentBuilder(_dependencyRetrieverMock.Object, _constructorMock.Object).BuildArguments();
+        }
+    }
+}
---
+++ Wingman/DI/DiArgumentBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace Wingman.DI
+{
+    using Wingman.Container;
+
+    internal class DiArgumentBuilder : ArgumentBuilderBase
+    {
+        private readonly IConstructor _constructor;
+
+        internal DiArgumentBuilder(IDependencyRetriever dependencyRetriever, IConstructor constructor) : base(dependencyRetriever, constructor)
+        {
+            _constructor = constructor;
+        }
+
+        private protected override void InstantiateAndFillArguments()
+        {
+            Arguments = new object[_constructor.ParameterCount];
+            ResolveDependenciesFromStart(Arguments.Length);
+        }
+    }
+}
